ae7dd108db985514229ef043ef4d669afa8eceae
DHARMIGACH <dharmiga.xworkz@gmail.com>
2024-07-23 19:41:56 +0530

    new keyword

---
+++ HelicopterRunner3.java
@@ -0,0 +1,108 @@
+public class HelicopterRunner3 {
+    public static void main(String[] args) {
+        System.out.println("Creating Helicopter instances:");
+        Helicopter h1 = new Helicopter();
+        Helicopter h2 = new Helicopter();
+        Helicopter h3 = new Helicopter();
+        Helicopter h4 = new Helicopter();
+        Helicopter h5 = new Helicopter();
+
+        System.out.println("\nCreating Market instances:");
+        Market m1 = new Market();
+        Market m2 = new Market();
+        Market m3 = new Market();
+        Market m4 = new Market();
+        Market m5 = new Market();
+
+        System.out.println("\nCreating Mall instances:");
+        Mall ma1 = new Mall();
+        Mall ma2 = new Mall();
+        Mall ma3 = new Mall();
+        Mall ma4 = new Mall();
+        Mall ma5 = new Mall();
+
+        System.out.println("\nCreating Train instances:");
+        Train t1 = new Train();
+        Train t2 = new Train();
+        Train t3 = new Train();
+        Train t4 = new Train();
+        Train t5 = new Train();
+
+        System.out.println("\nCreating Ring instances:");
+        Ring r1 = new Ring();
+        Ring r2 = new Ring();
+        Ring r3 = new Ring();
+        Ring r4 = new Ring();
+        Ring r5 = new Ring();
+
+        System.out.println("\nCreating Auto instances:");
+        Auto a1 = new Auto();
+        Auto a2 = new Auto();
+        Auto a3 = new Auto();
+        Auto a4 = new Auto();
+        Auto a5 = new Auto();
+
+        System.out.println("\nCreating Auditorium instances:");
+        Auditorium au1 = new Auditorium();
+        Auditorium au2 = new Auditorium();
+        Auditorium au3 = new Auditorium();
+        Auditorium au4 = new Auditorium();
+        Auditorium au5 = new Auditorium();
+
+        System.out.println("\nCreating SpaceShip instances:");
+        SpaceShip ss1 = new SpaceShip();
+        SpaceShip ss2 = new SpaceShip();
+        SpaceShip ss3 = new SpaceShip();
+        SpaceShip ss4 = new SpaceShip();
+        SpaceShip ss5 = new SpaceShip();
+
+        System.out.println("\nCreating Alien instances:");
+        Alien al1 = new Alien();
+        Alien al2 = new Alien();
+        Alien al3 = new Alien();
+        Alien al4 = new Alien();
+        Alien al5 = new Alien();
+
+        System.out.println("\nCreating Bullet instances:");
+        Bullet b1 = new Bullet();
+        Bullet b2 = new Bullet();
+        Bullet b3 = new Bullet();
+        Bullet b4 = new Bullet();
+        Bullet b5 = new Bullet();
+
+        System.out.println("\nCreating Galaxy instances:");
+        Galaxy g1 = new Galaxy();
+        Galaxy g2 = new Galaxy();
+        Galaxy g3 = new Galaxy();
+        Galaxy g4 = new Galaxy();
+        Galaxy g5 = new Galaxy();
+
+        System.out.println("\nCreating Spoon instances:");
+        Spoon s1 = new Spoon();
+        Spoon s2 = new Spoon();
+        Spoon s3 = new Spoon();
+        Spoon s4 = new Spoon();
+        Spoon s5 = new Spoon();
+
+        System.out.println("\nCreating BlackHole instances:");
+        BlackHole bh1 = new BlackHole();
+        BlackHole bh2 = new BlackHole();
+        BlackHole bh3 = new BlackHole();
+        BlackHole bh4 = new BlackHole();
+        BlackHole bh5 = new BlackHole();
+
+        System.out.println("\nCreating Cake instances:");
+        Cake c1 = new Cake();
+        Cake c2 = new Cake();
+        Cake c3 = new Cake();
+        Cake c4 = new Cake();
+        Cake c5 = new Cake();
+
+        System.out.println("\nCreating Wrapper instances:");
+        Wrapper w1 = new Wrapper();
+        Wrapper w2 = new Wrapper();
+        Wrapper w3 = new Wrapper();
+        Wrapper w4 = new Wrapper();
+        Wrapper w5 = new Wrapper();
+    }
+}
