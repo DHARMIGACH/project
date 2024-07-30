6380bd7729fa0a427edb3a9d497fb2ef8af8b61e
DHARMIGACH <dharmiga.xworkz@gmail.com>
2024-07-30 10:29:22 +0530

    Claw

---
+++ Claw1.java
@@ -0,0 +1,89 @@
+class Claw1 {
+    String name;
+    int noOfClaws;
+    String color = "brown";
+
+    Claw() {
+        System.out.println("Created Claw");
+    }
+}
+
+class Crow {
+    String name;
+    int noOfCrows;
+    String color = "black";
+
+    Crow() {
+        System.out.println("Created Crow");
+    }
+}
+
+class Clock {
+    String name;
+    int noOfClocks;
+    String color = "white";
+
+    Clock() {
+        System.out.println("Created Clock");
+    }
+}
+
+class Lollipop {
+    String name;
+    int noOfLollipops;
+    String color = "red";
+
+    Lollipop() {
+        System.out.println("Created Lollipop");
+    }
+}
+
+class Jean {
+    String name;
+    int noOfJeans;
+    String color = "blue";
+
+    Jean() {
+        System.out.println("Created Jean");
+    }
+}
+
+class Keyboard {
+    String name;
+    int noOfKeyboards;
+    String color = "black";
+
+    Keyboard() {
+        System.out.println("Created Keyboard");
+    }
+}
+
+class Mountain {
+    String name;
+    int noOfMountains;
+    String color = "green";
+
+    Mountain() {
+        System.out.println("Created Mountain");
+    }
+}
+
+class Stadium {
+    String name;
+    int noOfStadiums;
+    String color = "gray";
+
+    Stadium() {
+        System.out.println("Created Stadium");
+    }
+}
+
+class Spray {
+    String name;
+    int noOfSprays;
+    String color = "blue";
+
+    Spray() {
+        System.out.println("Created Spray");
+    }
+}
